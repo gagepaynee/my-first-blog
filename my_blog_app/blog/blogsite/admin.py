385dbdce650665a0578fd5a69d09e0f286281fba
Gage Payne <gage_payne37@yahoo.com>
2018-01-27 00:55:32 -0500

    My site app, first commit

---
+++ my_blog_app/blog/blogsite/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from blogsite.models import Post
+from blogsite.models import Comment
+# Register your models here.
+admin.site.register(Post)
+admin.site.register(Comment)
